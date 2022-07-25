7359db82d07fbc2f4df9361e8427ba1b0a2541b4
Výborný Adam <mail@adamvyborny.cz>
2022-07-25 13:57:53 +0200

    Actual processor core code and its tests

--- src/Component.php
+++ src/Component.php
@@ -5,12 +5,30 @@ declare(strict_types=1);
 namespace FixerApiErrorHandlingProcessor;
 
 use Keboola\Component\BaseComponent;
+use Keboola\Component\UserException;
+use RuntimeException;
+use SplFileInfo;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Process\Process;
 
 class Component extends BaseComponent
 {
+    /**
+     * @throws \Keboola\Component\UserException
+     */
     protected function run(): void
     {
-        // @TODO implement
+        $finder = new Finder();
+        $finder->notName('*.manifest')->in($this->getDataDir() . '/in/tables')->depth(0);
+        foreach ($finder as $sourceTable) {
+            $this->processFile($sourceTable, $this->getDataDir() . '/out/tables');
+        }
+
+        $finder = new Finder();
+        $finder->name('*.manifest')->in($this->getDataDir() . '/in/tables')->depth(0);
+        foreach ($finder as $sourceTableManifest) {
+            $this->processManifest($sourceTableManifest, $this->getDataDir() . '/out/tables');
+        }
     }
 
     protected function getConfigClass(): string
@@ -21,5 +39,55 @@ class Component extends BaseComponent
     protected function getConfigDefinitionClass(): string
     {
         return ConfigDefinition::class;
+    }
+
+    private function processManifest(SplFileInfo $sourceFile, string $destinationFolder): void
+    {
+        $process = new Process(
+            ['mv', $sourceFile->getPathname(), $destinationFolder . '/' . $sourceFile->getBasename()]
+        );
+        $process->mustRun();
+    }
+
+    /**
+     * @throws \Keboola\Component\UserException
+     */
+    private function processFile(SplFileInfo $sourceFile, string $destinationFolder): void
+    {
+        $source = fopen($sourceFile->getPathname(), 'r');
+        if ($source === false) {
+            throw new RuntimeException(sprintf('Failed to open source file: "%s"', $sourceFile->getPathname()));
+        }
+
+        $header = fgets($source);
+        if (!$header) {
+            throw new RuntimeException('Cannot obtain header row from CSV');
+        }
+        $columns = explode(',', trim(str_replace('"', '', $header)));
+        if (in_array('error_code', $columns)) {
+            $row = fgets($source);
+            if (!$row) {
+                throw new RuntimeException('Cannot obtain row from CSV');
+            }
+            $values = explode(',', trim(str_replace('"', '', $row)));
+            $data = [];
+            foreach ($columns as $key => $column) {
+                $data[$column] = $values[$key];
+            }
+
+            if ($data['error_code'] !== '') {
+                throw new UserException(sprintf(
+                    '%d: Fixer API error "%s": %s',
+                    $data['error_code'],
+                    $data['error_type'],
+                    $data['error_info']
+                ));
+            }
+        } else {
+            $process = new Process(
+                ['mv', $sourceFile->getPathname(), $destinationFolder . '/' . $sourceFile->getBasename()]
+            );
+            $process->mustRun();
+        }
     }
 }
--- src/Config.php
+++ src/Config.php
@@ -8,9 +8,4 @@ use Keboola\Component\Config\BaseConfig;
 
 class Config extends BaseConfig
 {
-    // @todo implement your custom getters
-    public function getFoo(): string
-    {
-        return $this->getStringValue(['parameters', 'foo']);
-    }
 }
